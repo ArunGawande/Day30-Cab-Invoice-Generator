620d1d311b97b3b9e8243cdb29b77f9e89fd1a48
Arun <arungawande1995@gmail.com>
2022-07-09 15:37:22 +0530

    Added: step3 to Enhanced Invoice

---
+++ src/main/java/com/bridgelabz/CabInvoiceMain.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class CabInvoiceMain 
+{
+
+	private static final int COST_PER_TIME=1;
+    private static final double MIN_COST_PER_KM=10;
+    private static final double MIN_FARE=5;
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+        return Math.max(totalFare, MIN_FARE);
+    }
+
+    public InVoiceSummary calculateFare(Ride[] ride1){
+        double  totalFare=0;
+        for (Ride ride:ride1){
+            totalFare+=this.calculateFare(ride.getDistance, ride.getTime);
+        }
+        return new InVoiceSummary(ride1.length, totalFare);
+    }
+}
---
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -0,0 +1,46 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceGeneratorTest 
+{
+		
+    CabInvoiceMain inVoiceGenerator = null;
+    private InVoiceSummary fare;
+
+    @Before
+    public void setUp() throws Exception {
+        inVoiceGenerator = new CabInvoiceMain();
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceMain inVoiceGenerator = new CabInvoiceMain();
+        double distance = 2.0;
+        int time = 5;
+        double fare = inVoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 25.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndFare_ShouldReturnMinFare() {
+        CabInvoiceMain inVoiceGenerator = new CabInvoiceMain();
+        double distance = 0.1;
+        int time = 1;
+        double fare = inVoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5.0, fare, 0.0);
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] ride1 = {new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        CabInvoiceMain inVoiceGenerator=new CabInvoiceMain();
+        InVoiceSummary inVoiceSummary = inVoiceGenerator.calculateFare(ride1);
+        InVoiceSummary expectedInVoiceSummary= new InVoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInVoiceSummary, inVoiceSummary);
+    }
+
+}
